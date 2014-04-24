3aed9609edfd3863ff1de0e7d244405f6d064c3e
James Westbury <icydesign@gmail.com>
2014-04-23 21:41:12 -0700

    minor logic fix

--- spawncamper.py
+++ spawncamper.py
@@ -51,7 +51,7 @@ def host_match(kind):
 			else:
 				print "%s is not within the target subnet." % name
 		
-if args.subnet and args.startip and args.endip:
+if args.subnet and (args.startip or args.endip):
 	print "Cannot use -c with -s or -e. Please use -c or -s and -e."
 	quit()
 elif args.subnet:
@@ -61,23 +61,3 @@ elif args.startip and args.endip:
 else:
 	print "Matching for entire zone %s. Press Enter if you're sure." % args.zone
 	raw_input()
-	
-
-#DESCRIPTION
-#	Queries a DNS server, performing an AXFR zone transfer to obtain a list
-#	of hosts which may need to be monitored by Nagios. Takes arguments
-#	for nameserver and DNS zone, and optional arguments to define 
-#
-#OPTIONS
-#	-z	: DNS zone to query (e.g. internal.company.com)
-#	-n	: Nameserver against which to perform query
-#	-c	: CIDR subnet in which monitored hosts reside. Cannot be used
-#		  in conjunction with options -s/e. (Default: All IPs.)
-#	-s/e	: Starting and ending IP addresses, defining a range in which
-#		  monitored hosts reside. Cannot be used in conjunction with
-#		  option -c. (Default: All IPs.)
-#	-h	: Nagios host configuration directory. nag_auto_add will not
-#		  add new hosts if they already exist in a file within this
-#		  directory. (Default: /usr/local/nagios/etc/objects)
-#	-m	: Send updates to this address when hosts are discovered.
-#"""
